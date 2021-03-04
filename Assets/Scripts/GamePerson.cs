1d49ccd50baa24a32b936a471c3c57bf5ef1fbdc
Aya <ayanka_m@mail.ru>
2021-03-04 13:42:22 +0300

    Old vers. with old design

---
+++ Assets/Scripts/GamePerson.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePerson : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject person = new GameObject { };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
